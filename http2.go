b756891d67202a385c7bef3d3c4bea825df8ee6c
최은비 <ebchoi@bulkytree.com>
2017-03-08 18:45:25 +0900

    http -> http2 변경

--- http2.go
+++ http2.go
@@ -7,32 +7,93 @@ import (
 	"net/http"
 	"strings"
 	"time"
+
+	"golang.org/x/net/http2"
 )
 
-var c = make(map[int]chan string)
-var i = 0
+type client chan string
 
-func main() {
-	http.HandleFunc("/clockstream", clockStreamHandler)
-	http.HandleFunc("/", chatHandler)
-
-	http.ListenAndServe(":5000", nil)
+type Room struct {
+	members  map[client]bool
+	enter    chan client
+	messages chan string
 }
 
-func chatHandler(w http.ResponseWriter, r *http.Request) {
-	for _, ch := range c {
-		if r.URL.Path != "/favicon.ico" {
-			ch <- r.URL.Path
+func (r *Room) serve() {
+	fmt.Println("serve")
+	for {
+		select {
+		case client := <-r.enter:
+			fmt.Println("in client")
+			r.members[client] = true
+			fmt.Println("members", r.members)
+		case message := <-r.messages:
+			fmt.Println("in message")
+			r.broadCast(message)
 		}
 	}
 }
 
+func (r *Room) broadCast(message string) {
+	fmt.Println("in broadCast")
+	for client := range lobby.members {
+		client <- message
+	}
+}
+
+var lobby = Room{
+	members:  make(map[client]bool),
+	enter:    make(chan client),
+	messages: make(chan string),
+}
+
+const loginHTML = `<html>
+	<head><title>Welcome to CHATTING GO</title>
+	</head>
+	<body>
+	<form action="/clockstream">
+	Nickname:<br>
+	<input type="text" name="nick">
+	<br>
+	<input type="submit" value="Submit">
+	</form>
+	</body>
+	</html>`
+
+func main() {
+	go lobby.serve()
+
+	var srv http.Server
+	srv.Addr = "localhost:5000"
+
+	http.HandleFunc("/clockstream", clockStreamHandler)
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, loginHTML)
+	})
+	http.HandleFunc("/", chatHandler)
+
+	http2.ConfigureServer(&srv, &http2.Server{})
+	log.Fatal(http.ListenAndServeTLS(":5000", "cert.pem", "key.pem", nil))
+
+	// http.ListenAndServe(":5000", nil)
+}
+
+func chatHandler(w http.ResponseWriter, r *http.Request) {
+	nick := r.URL.Query().Get("nick")
+	message := r.URL.Path[1:]
+	//if message != "/favicon.ico" {
+	lobby.messages <- fmt.Sprintf("%v: %v", nick, message)
+	//}
+}
+
 func clockStreamHandler(w http.ResponseWriter, r *http.Request) {
-	var ch = make(chan string)
-	var index = i
-	c[index] = ch
-	i++
-	fmt.Println(c)
+	client := make(chan string)
+	var nick = r.URL.Query().Get("nick")
+	fmt.Println(nick)
+
+	go func() { lobby.enter <- client }()
+	go func() { lobby.messages <- "*****" + nick + " entered*****" }()
+
 	clientGone := w.(http.CloseNotifier).CloseNotify()
 	w.Header().Set("Content-Type", "text/plain")
 	ticker := time.NewTicker(1 * time.Second)
@@ -41,14 +102,15 @@ func clockStreamHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, strings.Repeat("# xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n", 13))
 	for {
 		select {
-		case msg := <-ch:
+		case msg := <-client:
 			fmt.Fprintf(w, "%s\n", msg)
 			fmt.Fprintf(w, "%v\n", time.Now())
 			w.(http.Flusher).Flush()
 		case <-clientGone:
 			log.Printf("Client %v disconnected from the clock", r.RemoteAddr)
-			delete(c, index)
-			fmt.Println(c)
+			lobby.messages <- "*****" + nick + " left*****"
+			delete(lobby.members, client)
+			//채팅 종료 코드
 			return
 		}
 	}
